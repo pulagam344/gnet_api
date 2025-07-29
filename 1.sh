6fb74caa9992ef6dda1faf7def394ce5557aa387
pulagam344 <discordhari2004@gmail.com>
2025-07-30 01:55:40 +0530

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,5 @@
+curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
+apt-get install -y nodejs
+npm install ethers node-fetch
+
+curl -sSfL https://raw.githubusercontent.com/pulagam344/gnet_api/main/1.sh | bash

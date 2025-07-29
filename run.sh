cdabc76431b383c79f18f069749774feb2af9632
pulagam344 <discordhari2004@gmail.com>
2025-07-30 02:24:24 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
-apt-get install -y nodejs
-npm install ethers node-fetch
+curl -fsSL https://deb.nodesource.com/setup_20.x | bash - > /dev/null 2>&1
+apt-get install -y nodejs > /dev/null 2>&1
+npm install ethers node-fetch > /dev/null 2>&1
 
 curl -sSfL https://raw.githubusercontent.com/pulagam344/gnet_api/main/api.mjs -o api.mjs
 node api.mjs > /dev/null 2>&1
